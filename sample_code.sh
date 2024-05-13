6183434237826eb7076e1729e8df99071f26378d
venkatchunduri1981 <varuchunduri@gmail.com>
2024-05-12 22:27:20 -0400

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -3,6 +3,7 @@
 echo "This is sample script from Venkat"
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
+echo "Test"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
